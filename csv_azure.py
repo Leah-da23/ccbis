f531006288a0113672eb5295abcff617ee5e6087
qinli <leahql2018@gmail.com>
2023-04-30 19:07:54 +1000

    first commit

--- csv_azure.py
+++ csv_azure.py
@@ -1,2 +1,23 @@
-print('123')
-print('456')
+import csv
+import pyodbc
+
+server = 'da-trainingql.database.windows.net'
+database = 'da-trainingql'
+username = 'da-trainingql'
+password = '1qaz2wsxDa'
+driver = '{ODBC Driver 17 for SQL Server}'
+
+filename = 'DimAgent.csv'   
+delimiter = ','
+
+cnxn = pyodbc.connect('DRIVER=' + driver + ';SERVER=' + server + ';PORT=1433')
+
+cursor = cnxn.cursor()
+with open(DimAgent.csv, newline='') as csvfile:
+    reader = csv.reader(csvfile, delimiter=delimiter, quotechar='')
+    for row in reader:
+    
+        query = "INSERT INTO dbo.AgentSource (AgentID, FirstName, LastName) VALUE"
+        cursor.execute(query)
+cnxn.commit()
+cnxn.close()
